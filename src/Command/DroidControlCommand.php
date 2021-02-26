ceed2f095e9613e5eee9cde7b3e39a8b0f166a04
kestux <kestutis.kacinskas@airtimerewards.com>
2021-02-26 00:30:11 +0000

    [implementation] adding functionality to command

--- src/Command/DroidControlCommand.php
+++ src/Command/DroidControlCommand.php
@@ -2,13 +2,40 @@
 
 namespace App\Command;
 
+use App\Generator\DroidPathGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DroidControlCommand extends Command
 {
     protected static $defaultName = 'death-star.droid.control';
+
+    private const DEFAULT_DROID_STOCK = 100;
+
+    private const OPTION_STOCK_NAME_LONG = 'stock';
+    private const OPTION_STOCK_NAME_SHORT = 's';
+
+    private const RESPONSE_STATUS_EXPECTATION_FAILED = 417;
+    private const RESPONSE_STATUS_GONE = 410;
+    private const RESPONSE_STATUS_OK = 200;
+
+    private const STEP_FORWARD = 'f';
+    private const STEP_LEFT = 'l';
+    private const STEP_RIGHT = 'r';
+
+    private const DISPATCHER_RESULT_TO_PATH_GENERATOR_STATUS_MAP = [
+        self::RESPONSE_STATUS_EXPECTATION_FAILED => DroidPathGenerator::RESULT_CRASHED,
+        self::RESPONSE_STATUS_GONE => DroidPathGenerator::RESULT_LOST,
+        self::RESPONSE_STATUS_OK => DroidPathGenerator::RESULT_SUCCESS,
+    ];
+
+    private const PATH_GENERATOR_TO_DISPATCHER_STEP_MAP = [
+        DroidPathGenerator::STEP_FORWARD => self::STEP_FORWARD,
+        DroidPathGenerator::STEP_LEFT => self::STEP_LEFT,
+        DroidPathGenerator::STEP_RIGHT => self::STEP_RIGHT,
+    ];
 
     public function __construct(string $name = null)
     {
@@ -17,7 +44,14 @@ class DroidControlCommand extends Command
 
     protected function configure()
     {
-        $this->setDescription('Sentry droids to reach and reinforce the vulnerable point');
+        $this->setDescription('Sentry droids to reach and reinforce the vulnerable point')
+            ->addOption(
+                self::OPTION_STOCK_NAME_LONG,
+                self::OPTION_STOCK_NAME_SHORT,
+                InputOption::VALUE_OPTIONAL,
+                'The amount of droids in stock',
+                self::DEFAULT_DROID_STOCK
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
---
+++ tests/Command/DroidControlCommandTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Command;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \App\Command\DroidControlCommand
+ */
+class DroidControlCommandTest extends TestCase
+{
+
+}
